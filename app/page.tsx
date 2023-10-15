27384ee54c2aee7a046b345349633b6fbf7b0872
Jatin <jatin.ahluwalia.5@gmail.com>
2023-10-15 23:46:25 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,15 @@ import {
   CommandItem,
 } from "@/components/ui/command";
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
   Popover,
   PopoverContent,
   PopoverTrigger,
@@ -29,7 +38,46 @@ import {
 } from "lucide-react";
 import { useState } from "react";
 
-const locations = ["Delhi", "Mumbai"];
+const locations = ["delhi", "mumbai"];
+
+const flightss = [
+  {
+    from: "delhi",
+    to: "mumbai",
+    date: new Date(2023, 10, 16),
+    name: "Flight 1",
+  },
+  {
+    from: "delhi",
+    to: "mumbai",
+    date: new Date(2023, 10, 17),
+    name: "Flight 2",
+  },
+  {
+    from: "delhi",
+    to: "mumbai",
+    date: new Date(2023, 10, 18),
+    name: "Flight 3",
+  },
+  {
+    from: "mumbai",
+    to: "delhi",
+    date: new Date(2023, 10, 16),
+    name: "Flight 4",
+  },
+  {
+    from: "mumbai",
+    to: "delhi",
+    date: new Date(2023, 10, 17),
+    name: "Flight 5",
+  },
+  {
+    from: "mumbai",
+    to: "delhi",
+    date: new Date(2023, 10, 18),
+    name: "Flight 6",
+  },
+];
 
 const Home = () => {
   const [fromValue, setFromValue] = useState("");
@@ -37,6 +85,16 @@ const Home = () => {
   const [toValue, setToValue] = useState("");
   const [toOpen, setToOpen] = useState(false);
   const [dateValue, setDateValue] = useState<Date>();
+  const [flights, setFlights] = useState<typeof flightss | null>(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleFlights = () => {
+    const filteredFlights = flightss.filter(
+      (flight) => flight.from === fromValue && flight.to === toValue
+    );
+    setFlights(filteredFlights);
+  };
+
   return (
     <>
       <Card>
@@ -46,10 +104,10 @@ const Home = () => {
             Select your origin and destination here
           </CardDescription>
         </CardHeader>
-        <CardContent className="flex gap-5 items-center">
+        <CardContent className="flex gap-5 items-center max-sm:flex-col max-sm:items-stretch">
           <Popover open={fromOpen} onOpenChange={setFromOpen}>
             <PopoverTrigger asChild className="grow">
-              <Button variant={"outline"} className="w-[300px] justify-between">
+              <Button variant={"outline"} className="justify-between">
                 {fromValue ? fromValue.toLocaleUpperCase() : "From...."}
                 <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
               </Button>
@@ -67,17 +125,17 @@ const Home = () => {
                         setFromOpen(false);
                       }}
                     >
-                      {location}
+                      {location.toLocaleUpperCase()}
                     </CommandItem>
                   ))}
                 </CommandGroup>
               </Command>
             </PopoverContent>
           </Popover>
-          <ArrowRight />
+          <ArrowRight className="max-sm:hidden" />
           <Popover open={toOpen} onOpenChange={setToOpen}>
             <PopoverTrigger asChild className="grow">
-              <Button variant={"outline"} className="w-[300px] justify-between">
+              <Button variant={"outline"} className="justify-between">
                 {toValue ? toValue.toLocaleUpperCase() : "To...."}
                 <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
               </Button>
@@ -95,7 +153,7 @@ const Home = () => {
                         setToOpen(false);
                       }}
                     >
-                      {location}
+                      {location.toLocaleUpperCase()}
                     </CommandItem>
                   ))}
                 </CommandGroup>
@@ -105,10 +163,10 @@ const Home = () => {
           <Popover>
             <PopoverTrigger asChild>
               <Button
-                className="w-[300px] justify justify-between grow"
+                className="justify justify-between grow"
                 variant={"outline"}
               >
-                {dateValue?.toDateString() || "Select Date..."}
+                {dateValue?.toLocaleDateString() || "Select Date..."}
                 <CalendarIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
               </Button>
             </PopoverTrigger>
@@ -123,9 +181,65 @@ const Home = () => {
           </Popover>
         </CardContent>
         <CardFooter className="flex flex-col items-stretch">
-          <Button>Search Flights</Button>
+          <Button onClick={handleFlights}>Search Flights</Button>
         </CardFooter>
       </Card>
+      {flights && (
+        <Card className="bg-transparent text-white border-none">
+          <CardHeader>
+            <CardTitle>Flights</CardTitle>
+            <CardDescription className="text-gray-400">
+              {flights.length} flights found for {fromValue} to {toValue}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-5">
+            {flights.map((flight) => (
+              <Dialog
+                onOpenChange={setDialogOpen}
+                open={dialogOpen}
+                key={flight.name}
+              >
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{flight.name}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-col gap-5">
+                    <CardDescription>
+                      {flight.from.toLocaleUpperCase()} to{" "}
+                      {flight.to.toLocaleUpperCase()}
+                    </CardDescription>
+                    <CardDescription>
+                      {flight.date.toLocaleDateString()}
+                    </CardDescription>
+                  </CardContent>
+                  <CardFooter className="max-sm:flex-col max-sm:items-stretch">
+                    <DialogTrigger asChild>
+                      <Button>Book</Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Flight</DialogTitle>
+                        <DialogDescription>
+                          Your Flight Booked Successfully
+                        </DialogDescription>
+                      </DialogHeader>
+                      <DialogFooter>
+                        <Button
+                          onClick={() => {
+                            setDialogOpen(false);
+                          }}
+                        >
+                          Close
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </CardFooter>
+                </Card>
+              </Dialog>
+            ))}
+          </CardContent>
+        </Card>
+      )}
     </>
   );
 };
